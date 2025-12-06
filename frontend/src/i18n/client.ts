d241f0446cc763925643afc5f9634ca96bc56b9e
XezMet <office@xezmet.at>
2025-12-06 22:01:45 +0100

    Fix build error: Remove react-cookie dependency from i18n client

--- frontend/src/i18n/client.ts
+++ frontend/src/i18n/client.ts
@@ -2,10 +2,10 @@
 
 import i18next from 'i18next'
 import { initReactI18next, useTranslation as useTranslationOrg } from 'react-i18next'
-import { useCookies } from 'react-cookie'
 import resourcesToBackend from 'i18next-resources-to-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import { getOptions, languages, cookieName } from './settings'
+import { getOptions, languages } from './settings'
+import { useEffect } from 'react'
 
 const runsOnServerSide = typeof window === 'undefined'
 
@@ -18,27 +18,25 @@ i18next
     ...getOptions(),
     lng: undefined, // let detect the language on client side
     detection: {
-      order: ['path', 'htmlTag', 'cookie', 'navigator'],
+      order: ['path', 'htmlTag', 'navigator'],
     },
     preload: runsOnServerSide ? languages : []
   })
 
-export function useTranslation(lng: string, ns: string, options: any = {}) {
-  const [cookies, setCookie] = useCookies([cookieName])
+export function useTranslation(lng: string, ns: string = 'common', options: any = {}) {
   const ret = useTranslationOrg(ns, options)
   const { i18n } = ret
+  
   if (runsOnServerSide && lng && i18n.resolvedLanguage !== lng) {
     i18n.changeLanguage(lng)
   } else {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [activeLng, setActiveLng] = i18next.language === undefined ? [undefined, undefined] : [i18next.language, i18next.changeLanguage]
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    if (activeLng === i18next.language) return ret
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    if (activeLng !== lng) {
-        // @ts-ignore
-        setActiveLng(lng)
-    }
+    useEffect(() => {
+      if (i18n.resolvedLanguage !== lng) {
+        i18n.changeLanguage(lng)
+      }
+    }, [lng, i18n])
   }
+  
   return ret
 }
