3191a2c93ab29c2c7303d75a79f703c65898f397
XezMet <office@xezmet.at>
2026-01-17 20:20:55 +0100

    Simplify debug-api page to fix build errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // Improve error handling during build
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 5,
+  },
   images: {
     unoptimized: true,
     formats: ['image/webp', 'image/avif'],
